f589fcc149aaa7c82acf9ccf8c9eb7e3d6af7b73
lee <754775040@qq.com>
2016-08-30 14:04:35 +0800

    删除多余部分

--- js/h.js
+++
@@ -1,9 +0,0 @@
-+function(){
-	var t = (new Date()).getTime();
-	document.writeln('<link rel="stylesheet" href="bootstrap/css/simple-line-icons.css?'+t+'" type="text/css" />');  
-	document.writeln('<link rel="stylesheet" href="bootstrap/css/app.css?'+t+'" type="text/css" />  ');	     
-    document.writeln('<link rel="stylesheet" href="bootstrap/css/global.css?'+t+'" type="text/css" />  ');
-	document.writeln('<script src="js/plugin/MyAlert.js?'+t+'" type="text/javascript" charset="utf-8"></script>');
-	document.writeln('<script src="js/common.js?'+t+'" type="text/javascript" charset="utf-8"></script>');
-	document.writeln('<script src="js/PageLoad.js?'+t+'" type="text/javascript" charset="utf-8"></script>');
-}();
--- visitMainDomain.php
+++
@@ -1,6 +0,0 @@
-<?php
-// error_reporting(E_ALL);
-//研发环境
-include_once '../quba/index.php';
-//本地环境
-//include_once '../../quba/index.php';
